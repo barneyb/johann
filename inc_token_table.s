5a4bffc506dee4b1c19c8bd6bac435f7cec9db3a
Barney Boisvert <bboisvert@gmail.com>
2025-04-09 23:43:58 -0700

    narrow the token table so it can be used for imm values

--- inc_token_table.s
+++ inc_token_table.s
@@ -1,28 +1,28 @@
-; value token types             0x111000
-.set    T_BOOL      , 0x111000
-.set    T_CHAR      , 0x111001
-.set    T_ID        , 0x111002
-.set    T_INT       , 0x111003
-.set    T_STRING    , 0x111004
+; value token types             0x1000
+.set    T_BOOL      , 0x1000
+.set    T_CHAR      , 0x1001
+.set    T_ID        , 0x1002
+.set    T_INT       , 0x1003
+.set    T_STRING    , 0x1004
 ; literal keywords never become tokens, they become value tokens.
-; flow control keywords         0x110000
-.set    T_KW_AGAIN  , 0x110001
-.set    T_KW_DONE   , 0x110002
-.set    T_KW_FOR    , 0x110003
-.set    T_KW_IF     , 0x110004
-.set    T_KW_RETURN , 0x110005
-.set    T_KW_WHILE  , 0x110006
-; type keywords                 0x120000
-.set    T_KW_BOOL   , 0x120001
-.set    T_KW_CHAR   , 0x120002
-.set    T_KW_INT    , 0x120003
-.set    T_KW_VOID   , 0x120004
-; definition keywords           0x130000
-.set    T_KW_CLASS  , 0x130001
-.set    T_KW_FN     , 0x130002
-; dynamic allocation keywords   0x140000
-.set    T_KW_FREE   , 0x140001
-.set    T_KW_NEW    , 0x140002
+; flow control keywords         0x2000
+.set    T_KW_AGAIN  , 0x2001
+.set    T_KW_DONE   , 0x2002
+.set    T_KW_FOR    , 0x2003
+.set    T_KW_IF     , 0x2004
+.set    T_KW_RETURN , 0x2005
+.set    T_KW_WHILE  , 0x2006
+; type keywords                 0x3000
+.set    T_KW_BOOL   , 0x3001
+.set    T_KW_CHAR   , 0x3002
+.set    T_KW_INT    , 0x3003
+.set    T_KW_VOID   , 0x3004
+; definition keywords           0x4000
+.set    T_KW_CLASS  , 0x4001
+.set    T_KW_FN     , 0x4002
+; dynamic allocation keywords   0x5000
+.set    T_KW_FREE   , 0x5001
+.set    T_KW_NEW    , 0x5002
 ; ambiguous character tokens    ASCII (for now)
 .set    T_ASSIGN    , '='
 .set    T_BANG      , '!'
