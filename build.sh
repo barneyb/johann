aa5e90b6ee8a121f6cde265921c2bc48057ba406
Barney Boisvert <bboisvert@gmail.com>
2025-05-30 21:58:14 -0700

    make build.sh a little more aggro about safety

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 
+git restore bin/jnc lib/jstdlib.o
 make clean test all
 cp jnc/target/bin/jnc bin
 cp jstdlib/target/lib/jstdlib.o lib
-make not_quite_lisp
+if ! make clean test all not_quite_lisp; then
+    set +x
+    echo
+    echo "Failed to re-build"
+    echo
+    git restore bin/jnc lib/jstdlib.o
+    exit 1
+fi
